e3c4d5dea008393dd933853603855f72dc99909d
prajaktahanabar3 <prajaktahanabar0@gmail.com>
2025-04-04 19:53:46 +0530

    Add files via upload

---
+++ positivenagative.java
@@ -0,0 +1,20 @@
+package com.analytiq.crejava;
+
+public class positivenagative {
+
+	
+	public static void main(String[] args) {
+		int no=10;
+		if(no>0)
+		{
+			System.out.println("it is positive no....");	
+		}
+		else
+		{
+			System.out.println("it is negative no....");
+		}
+		
+		
+	}
+
+}
